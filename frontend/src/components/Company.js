e767b4f272f96ea3fc5ec239be57d636c85553e2
Segfaul <ikaruzds@gmail.com>
2023-09-07 18:33:40 +0300

    upd company styles

--- frontend/src/components/Company.js
+++ frontend/src/components/Company.js
@@ -4,15 +4,17 @@ import { Link } from 'react-router-dom';
 const SplitPic = ({className=''}) => {
     return (
         <div className='split-pic'>
-            <div className='split-pic-content'>
+            <div className='split-pic-text'>
+              <div className='split-pic-content'>
                 <div className='split-pic-content-header'>
                     <h3>Наши контакты</h3>
                     <p>111524 Москва, ул. Электродная 4Б, офис 302</p>
-                </div>
-                <div className='split-pic-links'>
-                    <div className='split-pic-link'><a href="tel:+ 8 (495) 220-22-20"><i class="fas fa-phone"></i><p>+ 8 (495) 220-22-20</p></a></div>
-                    <div className='split-pic-link'><a href="mailto:info@uc-rk.ru"><i class="fas fa-envelope"></i><p>info@uc-rk.ru</p></a></div>
-                </div>
+                  </div>
+                  <div className='split-pic-links contact-info-description-data'>
+                        <div><a href="tel:+ 8 (495) 220-22-20"><i class="fas fa-phone"></i><p>+ 8 (495) 220-22-20</p></a></div>
+                        <div><a href="mailto:info@uc-rk.ru"><i class="fas fa-envelope"></i><p>info@uc-rk.ru</p></a></div>
+                  </div>
+              </div>
             </div>
             <div className='split-pic-img' style={{ backgroundImage: `url(${require('../assets/about/test_tower.jpg')})`}}>
                 <i className='fas fa-share'></i>
@@ -36,9 +38,15 @@ const Company = () => {
           <div className='page-content'>
             <section className='uchebni-welcome'>
               <h3>ООО "УЦРК"</h3>
+              <p>
+                Учебный центр развития квалификации предоставляет услуги в области дополнительного профессионального образования(послевузовское образование).
+                <br />
+                Учебные занятия проводят высококвалифицированные преподаватели. Занятия могут быть организованы несколькими способами.
+                <br />
+              </p>
+              <SplitPic />
             </section>
           </div>
-          <SplitPic />
         </div>
     );
 };
