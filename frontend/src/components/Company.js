34cfc7466e679ae64001597dce188e2998564ee5
segfaul <ikaruzds@gmail.com>
2023-09-07 21:32:24 +0300

    upd company slider

--- frontend/src/components/Company.js
+++ frontend/src/components/Company.js
@@ -1,6 +1,70 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
+
+const SliderPartner = ({ className = '' }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const partners = [
+    { imageSrc: 'home/factory.png', alt: 'Партнер 1' },
+    { imageSrc: 'home/factory.png', alt: 'Партнер 2' },
+    { imageSrc: 'home/factory.png', alt: 'Партнер 3' },
+    { imageSrc: 'home/factory.png', alt: 'Партнер 4' },
+    { imageSrc: 'home/globe.png', alt: 'Партнер 1' },
+    { imageSrc: 'home/hand-shake.png', alt: 'Партнер 2' },
+    { imageSrc: 'home/lamp.png', alt: 'Партнер 3' },
+    { imageSrc: 'home/factory.png', alt: 'Партнер 4' },
+    { imageSrc: 'home/roundabout.png', alt: 'Партнер 1' },
+    { imageSrc: 'home/roundabout.png', alt: 'Партнер 2' },
+    { imageSrc: 'home/roundabout.png', alt: 'Партнер 3' },
+    { imageSrc: 'home/roundabout.png', alt: 'Партнер 4' },
+  ];
+
+  const itemsPerPage = 4;
+  const totalPages = Math.ceil(partners.length / itemsPerPage);
+
+  const handlePageIndicatorClick = (index) => {
+    setCurrentPage(index);
+  };
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPage((prevPage) => (prevPage + 1) % totalPages);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const renderPartnersOnPage = () => {
+    const startIndex = currentPage * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return partners.slice(startIndex, endIndex).map((partner, index) => (
+      <div className="partner" key={index}>
+        <img src={require('../assets/' + partner.imageSrc)} alt={partner.alt} />
+      </div>
+    ));
+  };
+
+  return (
+    <div className={`partners-slider ${className}`}>
+      <div className="sliderz-container">
+        <div className="slider">
+          {renderPartnersOnPage()}
+        </div>
+      </div>
+      <div className="slider-pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <div
+            key={index}
+            className={`page-indicator ${index === currentPage ? 'active' : ''}`}
+            onClick={() => handlePageIndicatorClick(index)}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const SplitPic = ({className=''}) => {
     return (
         <div className='split-pic'>
@@ -44,9 +108,20 @@ const Company = () => {
                 Учебные занятия проводят высококвалифицированные преподаватели. Занятия могут быть организованы несколькими способами.
                 <br />
               </p>
-              <SplitPic />
             </section>
           </div>
+          <SplitPic />
+          <div className='page-content'>
+            <section className='uchebni-welcome'>
+              <p>
+                Учебный центр развития квалификации предоставляет услуги в области дополнительного профессионального образования(послевузовское образование).
+                <br />
+                Учебные занятия проводят высококвалифицированные преподаватели. Занятия могут быть организованы несколькими способами.
+                <br />
+              </p>
+            </section>
+          </div>
+          <SliderPartner />
         </div>
     );
 };
